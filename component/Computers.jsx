c0ed44dbf98b3a07006036336c8919616c8cd99a
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-02 20:48:38 -0500

    changes with new component

--- component/Computers.jsx
+++ component/Computers.jsx
@@ -5,8 +5,21 @@ function Computers() {
   return (
     <div>
       <h1>Some MacBook types</h1>
-      <MacbookTypes specs={{ chip: "Apple M1 Pro", memory: "32 GB", macOS: "13.0" }} screensize={"14-inch"} />
-      <MacbookTypes specs={{ chip: "Apple M1 Pro", memory: "32 GB", macOS: "13.0" }} screensize={"14-inch"} />
+
+      <MacbookTypes
+        name={"Apple MacBook Pro"}
+        specs={" Apple M1 Pro chip"}
+        memory={"32 GB"}
+        screensize={"14-inch"}
+        img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-mbp14-silver-m1-2021?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1638576254000"
+      />
+      <MacbookTypes
+        name={"Apple MacBook Air"}
+        specs={" Apple M1 Pro chip"}
+        memory={"316 GB"}
+        screensize={"13-inch"}
+        img="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-mbp14-space-m1-2021?wid=1200&hei=1200&fmt=jpeg&qlt=95&.v=1638575247000"
+      />
     </div>
   );
 }
--- component/MacbookTypes.jsx
+++ component/MacbookTypes.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 
-function MacbookTypes({ specs, screensize }) {
+function MacbookTypes({ name, img, specs, screensize }) {
+  //First Step: Destructuring within the body of the function
+  //const { img, name, desc, price} = props ;
   return (
     <div>
-      <h2>MacBook Pro Max:</h2>
-      <img
-        src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-mbp14-silver-m1-2021?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1638576254000"
-        width="300"
-        height="300"
-        alt="macbookpro"
-        specs={specs}
-      />
-      <h4>14 inch 32 GB</h4>
+      <h5>Name: {name}</h5>
+      <img src={img} alt="macbook" width={200} height={200} />
+      <h5>Specs: {specs}</h5>
+      <h5>Screen: {screensize}</h5>
+
       <hr />
       {/* <h2>MacBook Pro:</h2>
       <h2>MacBook Air Max:</h2>
