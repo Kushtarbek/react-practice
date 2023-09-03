a2440871457c5c56c73b222218683f24a252ac00
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-02 20:47:08 -0500

    added new component with usestate hook

--- App.js
+++ App.js
@@ -1,14 +1,31 @@
-import ProfilePhoto from './component/Avatar';
-import Gallery from './component/Profile';
 import Computers from './component/Computers';
+import Gallery from './component/Sculptures';
+
+function Button ({text= "Click Me!", color="blue", fontSize="12", handleClick}) {
+
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + 'px'
+  }
+  return (<button onClick={() => handleClick('https://www.timer-tab.com/')} style={buttonStyle}>{text}</button>)
+}
 
 
 function App() {
+  const handleButtonClick = (url) => {
+    window.location.href = url
+  };
+
   return (
-    <div>
-     {/* <Gallery/>
-     <ProfilePhoto/> */}
-     <Computers/>
+    <div className='App'>
+    
+     
+     <Button handleClick={handleButtonClick}/>
+     <Button text="Don't Click Me!" color="red" fontSize={12}/>
+     <Button text="Click Me!" color="blue" fontSize={20}/>
+     <Computers />
+     <Gallery />
+    
     </div>
   );
 }
