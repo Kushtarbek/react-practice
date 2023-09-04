af7321b01f672ca4979c0569809b105f28f466b1
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-03 22:08:21 -0500

    added ButtonsGroup to App comp

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import Computers from './component/Computers';
 import Gallery from './component/Sculptures';
+import ButtonGroup from './component/Buttons';
 
 function Button ({text= "Click Me!", color="blue", fontSize="12", handleClick}) {
 
@@ -19,12 +20,14 @@ function App() {
   return (
     <div className='App'>
     
-     
+
      <Button handleClick={handleButtonClick}/>
      <Button text="Don't Click Me!" color="red" fontSize={12}/>
      <Button text="Click Me!" color="blue" fontSize={20}/>
      <Computers />
      <Gallery />
+     <ButtonGroup/>
+
     
     </div>
   );
