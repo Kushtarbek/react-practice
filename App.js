25d6ec827bb7c0ad0a379daf8584bf9adba74125
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-14 22:12:23 -0500

    carousel part 2

--- App.js
+++ App.js
@@ -4,6 +4,16 @@ import ButtonGroup from './component/Buttons';
 import Form from './component/InputFields';
 import Events from './component/RespondingToEvents';
 import LandingPage from './component/LandingPage';
+import Carousel from './component/Carousel';
+
+const images = [
+  "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
 
 function Button ({text= "Click Me!", color="blue", fontSize="12", handleClick}) {
 
@@ -41,6 +51,8 @@ function App() {
      <Events />
      </div> */}
      <LandingPage />
+
+     <Carousel images={images} />
      
     </div>
   ); 
--- component/Carousel.jsx
+++ component/Carousel.jsx
@@ -18,7 +18,32 @@ const Carousel = ({ images }) => {
   return (
     <div className="carousel">
       <img key={currentIndex} src={images[currentIndex]} />
-      <div className="slide_direction"></div>
+      <div className="slide_direction">
+        <div className="left" onClick={handlePrev}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+            <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+          </svg>
+        </div>
+
+        <div className="right" onClick={handleNext}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 96 960 960" width="20">
+            <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+          </svg>
+        </div>
+
+        <div className="indicator">
+          {images.map((_, index) => {
+            <div
+              key={index}
+              className={`dot ${currentIndex === index ? "active" : ""}`}
+              onClick={() => handleDotClick(index)}
+            ></div>;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
+
+export default Carousel;
+//https://www.freecodecamp.org/news/build-an-image-carousel-with-react-and-framer-motion/
