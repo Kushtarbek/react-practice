6f196be767e33bdf2b9bb724113f84450f80134a
kushtarbek <dr.kushtarbek@gmail.com>
2023-09-14 22:02:44 -0500

    carousel component part1

---
+++ component/Carousel.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Carousel = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1 === images.length ? 0 : prevIndex + 1));
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const handleDotClick = (index) => {
+    setCurrentIndex(index);
+  };
+
+  return (
+    <div className="carousel">
+      <img key={currentIndex} src={images[currentIndex]} />
+      <div className="slide_direction"></div>
+    </div>
+  );
+};
